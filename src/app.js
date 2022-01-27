658eeded6ce81e937351a1c61503d4ba5ab4ea49
kethllen <kety_andrey@hotmail.com>
2022-01-27 14:30:43 -0300

    connection error

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import express, {json} from 'express'
+import cors from 'cors'
+import { MongoClient } from 'mongodb';
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const server = express();
+server.use(cors())
+server.use(json());
+
+async function getDB(collection){
+    try {
+        const mongoClient = new MongoClient(process.env.MONGO_URI);
+        console.log("ainda nao conectei")
+        await mongoClient.connect();
+        console.log("agora eu ja conectei")
+        const dbApiChatUol = mongoClient.db("ApiChatUol")
+        const Collection = dbApiChatUol.collection(collection);
+    
+        return [mongoClient, dbApiChatUol, Collection]
+        
+    } catch (error) {
+        console.log(" dataBase Connection error")
+        console.log(error)
+    }
+
+}
+
+server.get('/participants', async (req,res) => {
+    try {
+          
+          const [mongoClient, dbApiChatUol, Collection] = await getDB("users");
+          const users = await Collection.find({}).toArray();
+                  
+          res.send(users)
+          mongoClient.close()
+       } catch (error) {
+          res.status(500).send('A culpa foi do estagiário')
+          //mongoClient.close()
+       }
+  });
+
+ server.listen(5000, () => {console.log("Servidor Inicializado")})
